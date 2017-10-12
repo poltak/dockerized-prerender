f9bc49ae9b28ee233790f5b42ce66a16ec8cdf8a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-10-12 15:40:57 +0700

    Write basic server with S3 support
    - mostly gotten from https://github.com/mondora/docker-prerender/blob/master/server.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const prerender = require('prerender')
+
+const server = prerender({})
+
+server.use(prerender.sendPrerenderHeader())
+server.use(prerender.removeScriptTags())
+
+if (process.env.ALLOWED_DOMAINS) {
+    server.use(prerender.whitelist())
+}
+if (process.env.BLACKLISTED_DOMAINS) {
+    server.use(prerender.blacklist())
+}
+
+// Html cache
+if (
+    process.env.AWS_ACCESS_KEY_ID &&
+    process.env.AWS_SECRET_ACCESS_KEY &&
+    process.env.S3_BUCKET_NAME
+) {
+    server.use(prerender.s3HtmlCache());
+} else {
+    server.use(prerender.inMemoryHtmlCache());
+}
+
+// Start
+server.start();
